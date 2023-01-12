c16f896d99ddf5e7437de8255985fe53dcb2f9db
Massimo Bonanni <massimo.bonanni@tiscali.it>
2023-01-12 13:28:46 +0100

    Add test class for RentOrchestrator

---
+++ Tests/ServerlessCarRent.Functions.Tests/DataGenerators/RentOrchestratorDataGenerator.cs
@@ -0,0 +1,51 @@
+﻿using ServerlessCarRent.Common.Dtos;
+using ServerlessCarRent.Common.Models.Car;
+using ServerlessCarRent.Common.Models.CarRental;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerlessCarRent.Functions.Tests.DataGenerators
+{
+    internal class RentOrchestratorDataGenerator
+    {
+        public static IEnumerable<object[]> GetData()
+        {
+            yield return new object[]
+            {
+                new RentOrchestratorDto()
+                {
+                     CarPlate="AA000AA",
+                     PickupLocation="LOCATION01",
+                     RentalStartDate=DateTimeOffset.Now,
+                     RenterEmail="jane.doe@mail.com",
+                     RenterFirstName="Jane",
+                     RenterLastName="Doe"
+                },
+                "1234567890",
+                true,
+                RentOperationState.Complete
+            };
+
+            yield return new object[]
+            {
+                new RentOrchestratorDto()
+                {
+                     CarPlate="AA000AA",
+                     PickupLocation="LOCATION01",
+                     RentalStartDate=DateTimeOffset.Now,
+                     RenterEmail="jane.doe@mail.com",
+                     RenterFirstName="Jane",
+                     RenterLastName="Doe"
+                },
+                "1234567890",
+                false,
+                RentOperationState.Error
+            };
+
+        }
+
+    }
+}
---
+++ Tests/ServerlessCarRent.Functions.Tests/Orchestrators/RentOrchestratorTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using Moq;
+using ServerlessCarRent.Common.Dtos;
+using ServerlessCarRent.Common.Interfaces;
+using ServerlessCarRent.Functions.Clients;
+using ServerlessCarRent.Functions.Entities;
+using ServerlessCarRent.Functions.Orchestrators;
+using ServerlessCarRent.Functions.Tests.DataGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerlessCarRent.Functions.Tests.Orchestrators
+{
+    public class RentOrchestratorTest
+    {
+        [Theory]
+        [MemberData(nameof(RentOrchestratorDataGenerator.GetData), MemberType = typeof(RentOrchestratorDataGenerator))]
+        public async void RunOrchestrator_Test(RentOrchestratorDto orchestratorDto, string instanceId,
+            bool pickupLocationRentResult, RentOperationState expectedOrchestratorResult)
+        {
+            // Setup Ilogger
+            var loggerMock = new Mock<ILogger<RentOrchestrator>>();
+
+            // Setup IDurableOrchestrationContext
+            var durableContextMock = new Mock<IDurableOrchestrationContext>();
+            durableContextMock.Setup(e => e.GetInput<RentOrchestratorDto>())
+                .Returns(orchestratorDto);
+            var pickupLocationEntityId = new EntityId(nameof(PickupLocationEntity), orchestratorDto.PickupLocation);
+            durableContextMock.Setup(e => e.CallEntityAsync<bool>(pickupLocationEntityId, nameof(IPickupLocationEntity.RentCar), It.IsAny<RentCarPickupLocationDto>()))
+                .Returns(Task.FromResult(pickupLocationRentResult));
+            durableContextMock.SetupGet(e => e.InstanceId)
+                .Returns(instanceId);
+
+            var orchestratorUT = new RentOrchestrator(loggerMock.Object);
+
+            var actual = await orchestratorUT.RunOrchestrator(durableContextMock.Object);
+
+            Assert.NotNull(actual);
+            Assert.Equal(orchestratorDto.CarPlate, actual.CarPlate);
+            Assert.Equal(orchestratorDto.PickupLocation, actual.PickupLocation);
+            Assert.Equal(expectedOrchestratorResult, actual.Status);
+            Assert.Equal(instanceId, actual.RentalId);
+        }
+    }
+}
