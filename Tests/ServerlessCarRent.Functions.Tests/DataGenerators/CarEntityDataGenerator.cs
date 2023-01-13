e1a0a41aad95cab9cdc7ce463072dccc09214b25
Massimo Bonanni <massimo.bonanni@tiscali.it>
2023-01-13 16:06:29 +0100

    Add test for CarEntity

---
+++ Tests/ServerlessCarRent.Functions.Tests/DataGenerators/CarEntityDataGenerator.cs
@@ -0,0 +1,42 @@
+﻿using ServerlessCarRent.Common.Dtos;
+using ServerlessCarRent.Common.Models.Car;
+using ServerlessCarRent.Common.Models.CarRental;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerlessCarRent.Functions.Tests.DataGenerators
+{
+    internal class CarEntityDataGenerator
+    {
+        public static IEnumerable<object[]> GetInitializeCarDtos()
+        {
+            yield return new object[]
+            {
+                "AA000BB",
+                new InitializeCarDto()
+                {
+                    CarStatus = CarState.Working,
+                    CostPerHour = 10M,
+                    Currency = "EUR",
+                    Model = "Fiat 500",
+                    PickupLocation = "MILAN"
+                }
+            };
+            yield return new object[]
+            {
+                "CC111DD",
+                new InitializeCarDto()
+                {
+                    CarStatus = CarState.Working,
+                    CostPerHour = 10M,
+                    Currency = "EUR",
+                    Model = "Fiat 500",
+                    PickupLocation = null
+                }
+            };
+        }
+    }
+}
---
+++ Tests/ServerlessCarRent.Functions.Tests/Entities/CarEntitytest.cs
@@ -0,0 +1,49 @@
+﻿using ServerlessCarRent.Common.Dtos;
+using ServerlessCarRent.Functions.Tests.DataGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Microsoft.Extensions.Logging;
+using ServerlessCarRent.Functions.Orchestrators;
+using ServerlessCarRent.Functions.Entities;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using ServerlessCarRent.Common.Interfaces;
+
+namespace ServerlessCarRent.Functions.Tests.Entities
+{
+    public class CarEntitytest
+    {
+        [Theory]
+        [MemberData(nameof(CarEntityDataGenerator.GetInitializeCarDtos), MemberType = typeof(CarEntityDataGenerator))]
+        public void Initialize_Test(string carPlate, InitializeCarDto initDto)
+        {
+            // Setup Ilogger
+            var loggerMock = new Mock<ILogger<CarEntity>>();
+            // Setup IDurableEntityContext
+            var entityContextMock = new Mock<IDurableEntityContext>();
+            entityContextMock.SetupGet(e => e.EntityKey).Returns(carPlate);
+            if (!string.IsNullOrEmpty(initDto.PickupLocation))
+            {
+                var pickupLocationEntityId = new EntityId(nameof(PickupLocationEntity), initDto.PickupLocation);
+                entityContextMock.Setup(e => e.SignalEntity(pickupLocationEntityId,
+                        nameof(IPickupLocationEntity.CarStatusChanged), It.IsAny<CarStatusChangeDto>()))
+                    .Verifiable();
+            }
+            Entity.SetMockContext(entityContextMock.Object);
+
+            var entityUT = new CarEntity(loggerMock.Object);
+
+            entityUT.Initialize(initDto);
+
+            Assert.NotNull(entityUT.Status);
+            Assert.Equal(initDto.CarStatus, entityUT.Status.CurrentState);
+            Assert.Equal(initDto.Model, entityUT.Status.Model);
+            Assert.Equal(initDto.Currency, entityUT.Status.Currency);
+            Assert.Equal(initDto.PickupLocation, entityUT.Status.PickupLocation);
+            Assert.Equal(initDto.CostPerHour, entityUT.Status.CostPerHour);
+        }
+    }
+}
